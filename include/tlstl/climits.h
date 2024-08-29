2d13f0f109252c108f659e70ce6a345393966cf8
triangly <triangly@outlook.com>
2024-08-29 14:05:01 +0300

    climits

---
+++ include/tlstl/climits.h
@@ -0,0 +1,6 @@
+#ifndef __TLSTL_CLIMITS_H__
+#define __TLSTL_CLIMITS_H__
+
+#include <limits.h>
+
+#endif // __TLSTL_CLIMITS_H__
--- include/tlstl/utility.h
+++ include/tlstl/utility.h
@@ -1,5 +1,6 @@
 #ifndef __TLSTL_UTILITY_H__
 #define __TLSTL_UTILITY_H__
 
+#include <tlstl/__utility/declval.h>
 
 #endif // __TLSTL_UTILITY_H__
